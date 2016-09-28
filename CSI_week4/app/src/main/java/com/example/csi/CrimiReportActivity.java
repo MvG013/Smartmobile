329a663ac954d1d35026309edc8ae5a8af5e03ae
MvG013 <marcvangool@hotmail.com>
2016-09-28 12:34:05 +0200

    opdracht 5

--- CSI_week4/app/src/main/java/com/example/csi/CrimiReportActivity.java
+++ CSI_week4/app/src/main/java/com/example/csi/CrimiReportActivity.java
@@ -1,18 +1,26 @@
 package com.example.csi;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.List;
 
-public class CrimiReportActivity extends Activity{
+public class CrimiReportActivity extends Activity implements LocationListener{
 
 
     public Button Back;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,9 @@ public class CrimiReportActivity extends Activity{
         ImageView image = (ImageView) findViewById(R.id.imageView);
         image.setImageDrawable(criminal.mugshot);
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,2000, 1, this);
+
         Back.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -34,6 +45,41 @@ public class CrimiReportActivity extends Activity{
                 startActivity(intent);
             }
         });
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        String msg = "New Latitude: " + location.getLatitude()
+                + "New Longitude: " + location.getLongitude();
+
+        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+
+        //vergelijking moet nog gemaakt worden alleen tijd nood(p uitreiking)
+        Vibrator v = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        v.vibrate(500);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    //Geen tijd meer voor jammer genoeg
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+        Toast.makeText(getBaseContext(), "Gps is turned on!! ",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent);
+        Toast.makeText(getBaseContext(), "Gps is turned off!! ",
+                Toast.LENGTH_SHORT).show();
 
     }
 }
